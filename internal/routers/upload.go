5cd0dea7d86feb735e887d6954c061036c1f4cc7
sky <doifusd@163.com>
2021-05-31 06:29:09 +0800

    save

--- internal/routers/upload.go
+++ internal/routers/upload.go
@@ -7,9 +7,6 @@ import (
 	"blog/pkg/convert"
 	"blog/pkg/errcode"
 	"blog/pkg/upload"
-	"blog/pkg/util"
-	"fmt"
-	"path"
 
 	"github.com/gin-gonic/gin"
 )
@@ -50,7 +47,7 @@ func (u Upload) UploadFile(c *gin.Context) {
 
 func (u Upload) UploadFileMuli(c *gin.Context) {
 	resp := app.NewResponse(c)
-	//最大上传大小
+	//最大上传大小32M
 	err := c.Request.ParseMultipartForm(32 << 20)
 	//超过上传大小
 	if err != nil {
@@ -59,55 +56,36 @@ func (u Upload) UploadFileMuli(c *gin.Context) {
 		return
 	}
 	fhs := c.Request.MultipartForm.File["file"]
-	allowExt := global.AppSetting.UploadImageAllowExts
-	for _, fheader := range fhs {
-		// saveUploadImage(fheader)
-		fmt.Println(fheader.Filename)
-		//获取文件类型
-		fileExt := path.Ext(fheader.Filename)
-		fmt.Println(fileExt)
-		if util.InArray(fileExt, allowExt) == false {
-			resp.ToErrorResponse(errcode.ErrorUploadExtFail)
-			return
-		}
-		//
+	// allowExt := global.AppSetting.UploadImageAllowExts
+
+	fileType := convert.StrTo(c.PostForm("type")).MustInt()
+
+	if fileType <= 0 {
+		resp.ToErrorResponse(errcode.IntvalidParams)
+		return
 	}
 
-	// svc := service.New(c.Request.Context())
-	//fileInfo, err := svc.UploadFile(upload.FileType(fileType), file, fileHeader)
-	//if err != nil {
-	//	global.Logger.Errorf("svc.UPloadFile err: %v", err)
-	//	errsp := errcode.ErrorUploadFileFail.WithDetails(err.Error())
-	//	resp.ToErrorResponse(errsp)
-	//	return
-	//}
-	//resp.ToResponse(gin.H{
-	//	"file_access_url": fileInfo.AccessUrl,
-	//})
-
-	/*
-		file, fileHeader, err := c.Request.FormFile("file")
-		fileType := convert.StrTo(c.PostForm("type")).MustInt()
-		if err != nil {
-			errsp := errcode.IntvalidParams.WithDetails(err.Error())
-			resp.ToErrorResponse(errsp)
-			return
-		}
-		if fileHeader == nil || fileType <= 0 {
-			resp.ToErrorResponse(errcode.IntvalidParams)
-			return
-		}
-
+	var fileInfoItems = []string{}
+	for _, fheader := range fhs {
+		//获取文件类型
+		// fileExt := path.Ext(fheader.Filename)
+		//只限制图片
+		// if util.InArray(fileExt, allowExt) == false {
+		// resp.ToErrorResponse(errcode.ErrorUploadExtFail)
+		// return
+		// }
+		f, err := fheader.Open()
 		svc := service.New(c.Request.Context())
-		fileInfo, err := svc.UploadFile(upload.FileType(fileType), file, fileHeader)
+		fileInfo, err := svc.UploadFile(upload.FileType(fileType), f, fheader)
 		if err != nil {
 			global.Logger.Errorf("svc.UPloadFile err: %v", err)
 			errsp := errcode.ErrorUploadFileFail.WithDetails(err.Error())
 			resp.ToErrorResponse(errsp)
 			return
 		}
-		resp.ToResponse(gin.H{
-			"file_access_url": fileInfo.AccessUrl,
-		})
-	*/
+		fileInfoItems = append(fileInfoItems, fileInfo.AccessUrl)
+	}
+	resp.ToResponse(gin.H{
+		"file_access_url": fileInfoItems,
+	})
 }
